b3bc376a0f66b10a6e36ddbbbed7cbf97d882c17
Mike Chevett <mchevett@gmail.com>
2013-07-02 11:43:08 -0400

    gzip if you got em

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-var http = require("http"),
-	cluster = require("cluster"),
+var http = require('http'),
+	cluster = require('cluster'),
 	static = require('node-static'),
-	file = new(static.Server)('./public');
+	file = new(static.Server)('./public', {
+		gzip:true
+	}),
+	os = require('os'),
+	util = require('util')
 
-var numCPUs = require("os").cpus().length;
+
+
+var numCPUs = os.cpus().length;
 var port = parseInt(process.argv[2] || 3000);
 
 console.log('going...')
@@ -20,7 +26,16 @@ if (cluster.isMaster) {
 	console.log('gone.');
 	http.createServer(function (request, response) {
 		request.addListener('end', function () {
-			file.serve(request, response);
+			file.serve(request, response, function (err, result) {
+				if (err){
+					if (err.status === 404 || err.status === 500) {
+						file.serveFile(util.format('/%d.html', err.status), err.status, {}, request, response);
+					} else {
+						response.writeHead(err.status, err.headers);
+						response.end();
+					}
+				}
+			});
 		}).resume();
 	}).listen(port);
 }
