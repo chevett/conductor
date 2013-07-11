a01089d0a8d3ae99e3c2d4d0d6db8ff39d23bf50
Mike Chevett <mchevett@gmail.com>
2013-07-11 00:27:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,13 +6,27 @@ var http = require('http'),
 	}),
 	os = require('os'),
 	util = require('util')
-
-
-
+	
+var cache = Object.create(null);
 var numCPUs = os.cpus().length;
 var port = parseInt(process.argv[2] || 3000);
 
 console.log('going...')
+
+
+
+function _createCacheKey(request){
+	try {
+		return decodeURI(url.parse(request.url).pathname);
+	}
+	catch(e) {
+		return null;
+	}
+}
+
+function _needsBody(request){
+	return true;
+}
 
 if (cluster.isMaster) {
 	for (var i = 0; i < numCPUs; i++) {
@@ -25,9 +39,43 @@ if (cluster.isMaster) {
 } else {
 	console.log('gone.');
 	http.createServer(function (request, response) {
+		
+		
 		request.addListener('end', function () {
+			var cacheKey = _createCacheKey(request),
+				cacheItem = cache[cacheKey];
+			 
 			process.stdout.write(".");
+			
+			if (cacheItem){
+				if (_needsBody(request)){
+					if (cacheItem.hasBody){
+						response.writeHead(200, cacheItem.headersFor200);
+						response.write(cacheItem.body);
+						response.end();
+						return;
+					}
+				} else {
+					response.writeHead(304, cacheItem.headersFor304);
+					response.end();
+					return;
+				}
+			}
+			
 			file.serve(request, response, function (err, result) {
+				var newCacheItem = result.cacheItem;
+				if (newCacheItem){
+					if (!cacheItem || newCacheItem.hasBody || !cacheItem.hasBody){
+						cache[cacheKey] = result.cacheItem;
+				
+						setTimeout(function(){
+							// so under very heavy load we stop clearing the cache, right?
+							delete cache[cacheKey];
+						}, 60*1000);
+					}
+				}
+				
+				
 				if (err){
 					if (err.status === 404 || err.status === 500) {
 						file.serveFile(util.format('/%d.html', err.status), err.status, {}, request, response);
